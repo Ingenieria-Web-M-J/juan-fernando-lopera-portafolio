784ebc9e008c6e7a17617084a38db67d84568219
Juan Fernando Lopera Muñoz <juan.loperam@udea.edu.co>
2024-03-29 22:11:44 -0500

    feat: organismo para información de conocimientos agregado

---
+++ src/app/components/organisms/CentralSide/KnowledgeInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { KnowledgeCard } from '../../molecules'
+import { Title } from '../../atoms'
+
+const Knowledge = () => {
+    return (
+        <>
+            <Title title='Knowledge' size='text-3xl'/>
+            <div className="w-full grid grid-cols-3 p-5 gap-5">
+                <KnowledgeCard icon='icon-[mdi--server]' title='Backend' text='API Development, Server-side' />
+                <KnowledgeCard icon='icon-[mdi--database]' title='Databases' text='SQL, Data Modeling' />
+                <KnowledgeCard icon='icon-[mdi--git]' title='Good Practices' text='Version control, SOLID Patterns, CI/CD' />
+            </div>
+        </>
+    )
+}
+
+export default Knowledge
